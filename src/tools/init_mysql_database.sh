a442757697e9e16a450611a4c0a1cd32de74bf49
Quynh Xuan Nguyen <seriquynh@gmail.com>
2019-10-16 17:24:14 +0700

    Add init_mysql_database.sh

---
+++ src/tools/init_mysql_database.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+echo 'Initialize a new database'
+
+echo 'What is the username'?
+read USERNAME
+echo 'What is the password'?
+read PASSWORD
+echo 'What is the database'?
+read DATABASE
+
+COMMAND="CREATE USER '$USERNAME'@'localhost' IDENTIFIED BY '$PASSWORD'; CREATE DATABASE \`$DATABASE\`; GRANT ALL PRIVILEGES ON \`$DATABASE\`.* TO '$USERNAME'@'localhost'"
+
+sudo mysql -u root -p -e "$COMMAND"
+
+EXIT_CODE=$?
+
+if [ $EXIT_CODE != 0 ]; then
+    echo 'Could not initialize a new database!'
+    exit $EXIT_CODE
+fi
+
+echo 'Initialize a new database successfully'
+echo "MYSQL USERNAME: $USERNAME"
+echo "MYSQL PASSWORD: ***"
+echo "MYSQL DATABASE: $DATABASE"
