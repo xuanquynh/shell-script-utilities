ea32aa705eef21d9700ef3058690cde48bb19ffe
Quynh Xuan Nguyen <seriquynh@gmail.com>
2020-08-22 07:50:57 +0700

    Add help command

--- src/tools/get-docker-container-ip.sh
+++ src/tools/get-docker-container-ip.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
+if [ "$1" = "--help" ]; then
+    echo "Run \"utils get-docker-container-ip <container_id_or_name>\" to get the container ip"
+    echo ""
+    echo "For Example:"
+    echo "  utils get-docker-container-ip my_nginx_proxy"
+    exit 0
+fi
+
 docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $1
